5b6d4b385782f1933c92a57ca519842449e5b962
Marcelo Bomfim <bomfim.m@icloud.com>
2026-03-31 21:51:55 -0300

    awt add

--- scr/Calculator.java
+++ scr/Calculator.java
@@ -1,7 +1,7 @@
 //  Created by Marcelo Bomfim on 30/03/26.
 
 // Como é a primeira vez que uso Swing para programar interface gráfica boa parte do código terá comentários
-
+import java.awt.*;
 import javax.swing.*;
 
 public class Calculator {
@@ -9,8 +9,8 @@ public class Calculator {
         Jframe jframe = new Jframe("Calculator"); // Cria a janela com o título "Calculator"
         JTextField display = new JTextField(); // Cria o campo de texto que será o visor da calculadora
 
-        frame.setSize(300, 400); // Define o tamanho da janela: 300 de largura e 400 de altura
-        frame.SetDefaultCloseOperation(Jframe.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+        frame.SetDefaultCloseOperation(Jframe.EXIT_ON_CLOSE); // Fecha o programa ao fechar a janela
         frame.setLayout(null);
 
         display.setBounds(30, 30, 220, 40);
