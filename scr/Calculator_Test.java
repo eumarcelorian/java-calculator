c87894c2421717a3ce4ffc90ac59e28d770a96ff
Marcelo Bomfim <bomfim.m@icloud.com>
2026-04-15 20:21:42 -0300

    class Calculator_Test.java created

---
+++ scr/Calculator_Test.java
@@ -0,0 +1,3 @@
+// Arquivo criado para fazer testes unitários.
+/* Testes unitários são testes automatizados que verificam a menor parte testável de um software
+(uma função, método ou classe) de forma isolada */
